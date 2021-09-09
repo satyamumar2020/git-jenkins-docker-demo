c0d876c31f0e139ab893d2740591e199359ca0c6
satyam umarvaishya <satyamumar2020>
2021-09-09 18:51:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# git-jenkins-docker-demo" 
--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -10,4 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    public static void nextFunc()
+    {
+    	System.out.println( "Hello World!" );
+    }
 }
